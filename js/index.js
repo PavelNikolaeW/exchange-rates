db0a5e1a63dac11988d4cfa0d1d20d11e2de068f
PavelNikolaeW <pavel19.nikolaev93@gmail.com>
2022-03-24 18:19:17 +0300

    fix

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function include(url) {
-    var script = document.createElement('script');
-    script.src = url;
-    document.getElementsByTagName('body')[0].appendChild(script);
-}
-include("./js/CBR_XML_Daily_Ru.js");
